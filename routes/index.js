17a21c84c74633284d67e480788f21b8facf37b2
Enze <enzewu5@gmail.com>
2016-01-25 00:05:20 -0500

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,1 +1,95 @@
 
+var express = require('express');
+var router = express.Router();
+var mongoose = require('mongoose');
+
+//initialize mongoose
+mongoose.connect('mongodb://localhost/todo_development');
+var Schema = mongoose.Schema;
+var ObjectId = Schema.ObjectId;
+
+var Task = new Schema({
+  task: String
+});
+
+var Task = mongoose.model('Task', Task);
+
+/* GET home page. */
+router.get('/', function(req, res){
+  res.render('index', { title: 'Express' })
+});
+
+/* GET tasks page. */
+router.get('/tasks', function(req, res){
+    Task.find({}, function (err, docs) {    
+    res.render('tasks/index', {
+      title: 'Todos index view',
+      docs: docs
+    });
+  });
+});
+
+
+/* GET new task page. */
+router.get('/tasks/new', function(req, res){
+  res.render('tasks/new.jade', {
+    title: 'New Task'
+  });
+});
+
+// back to tasks page
+router.post('/tasks',function(req, res){
+  var task = new Task(req.body.task);
+  task.save(function (err) {
+    if (!err) {
+        
+      req.flash('info', 'Task created');
+      console.log(req.flash('info', 'Task created'));
+      res.redirect('/tasks');
+      
+    }
+    else {
+      req.flash('warning', err);
+      res.redirect('/tasks/new');
+    }
+  });
+});
+
+
+router.get('/tasks/:id/edit', function(req, res){
+  Task.findById(req.params.id, function (err, doc){
+    res.render('tasks/edit', {
+      title: 'Edit Task View',
+      task: doc
+    });
+  });
+});
+
+//update tasks 
+router.route('/tasks/:id').put(function(req, res){
+    console.log("enter into put");
+  Task.findById(req.params.id, function (err, doc){
+      console.log("enter into put");
+    doc.updated_at = new Date();
+    doc.task = req.body.task.task;
+    doc.save(function(err) {
+      if (!err){
+        res.redirect('/tasks');
+      }
+      else {
+        console.err(err);
+      }
+    });
+  });
+});
+
+//delete tasks
+router.delete('/tasks/:id', function(req, res){
+  Task.findOne({ _id: req.params.id }, function(err, doc) {
+    doc.remove(function() {
+      res.redirect('/tasks');
+    });
+  });
+});
+
+module.exports = router;
